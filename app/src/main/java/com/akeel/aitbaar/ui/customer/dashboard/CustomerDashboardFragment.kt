0deabb8c4a947d45d7527177b0ff03d7dfd66697
Akeel shafi <akeelshafi20@gmail.com>
2026-02-06 21:13:08 +0530

    working on vendor dashboard

--- app/src/main/java/com/akeel/aitbaar/ui/customer/dashboard/CustomerDashboardFragment.kt
+++ app/src/main/java/com/akeel/aitbaar/ui/customer/dashboard/CustomerDashboardFragment.kt
@@ -1,60 +1,59 @@
 package com.akeel.aitbaar.ui.customer.dashboard
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.akeel.aitbaar.R
+import com.akeel.aitbaar.ui.vendor.dashboard.VendorAdapter
+import com.akeel.aitbaar.ui.vendor.dashboard.VendorModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CustomerDashboardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CustomerDashboardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_dashboard, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CustomerDashboardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CustomerDashboardFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val view = inflater.inflate(R.layout.fragment_customer_dashboard, container, false)
+
+        val rvVendors = view.findViewById<RecyclerView>(R.id.rvVendors)
+
+        // ✅ Dummy Vendor List (Testing)
+        val vendorList = listOf(
+            VendorModel(
+                "AT",
+                "Ahmad Traders",
+                "Rs 3,200 Due | Today",
+                "Rs 3,200",
+                Color.parseColor("#FFD7A3")
+            ),
+            VendorModel("FS", "Fatima Store", "Rs 4,300 Overdue | 2 days ago", "Rs 4,300", Color.parseColor("#B7D6C0")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+            VendorModel("SE", "Suleman Electronics", "Rs 1,000 Due Tomorrow", "Rs 1,000", Color.parseColor("#A8C6FF")),
+        )
+
+        // ✅ LayoutManager + Adapter
+        rvVendors.layoutManager = LinearLayoutManager(requireContext())
+        rvVendors.adapter = VendorAdapter(vendorList)
+
+        return view
     }
 }
