f19c265d3a505a4945d5e1f1f0335606943d10e0
Akeel shafi <akeelshafi20@gmail.com>
2026-02-12 22:03:13 +0530

    working on vendor side application

---
+++ app/src/main/java/com/akeel/aitbaar/data/repository/TransactionRepository.kt
@@ -0,0 +1,57 @@
+package com.akeel.aitbaar.data.repository
+
+import android.content.Context
+import com.akeel.aitbaar.data.local.db.AppDatabase
+import com.akeel.aitbaar.data.local.entity.TransactionEntity
+import com.akeel.aitbaar.data.model.Status
+import com.akeel.aitbaar.data.model.Transaction
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+
+object TransactionRepository {
+
+    private lateinit var dao: com.akeel.aitbaar.data.local.dao.TransactionDao
+
+    // 🔹 Initialize database (call once in app)
+    fun init(context: Context) {
+        val db = AppDatabase.getDatabase(context)
+        dao = db.transactionDao()
+    }
+
+    // 🔹 Insert transaction into Room
+    suspend fun addTransaction(transaction: Transaction) {
+        val entity =
+            TransactionEntity(
+            customerName = transaction.customerName,
+            item = transaction.item,
+            amount = transaction.amount,
+            date = transaction.date,
+            status = transaction.status
+        )
+        dao.insertTransaction(entity)
+    }
+
+    // 🔹 Update transaction status
+    suspend fun updateTransactionStatus(id: Int, newStatus: Status) {
+        dao.updateTransactionStatus(id, newStatus)
+    }
+
+
+    // 🔹 Get all transactions from Room
+    fun getAllTransactions(): Flow<List<Transaction>> {
+        return dao.getAllTransactions().map { list ->
+            list.map { entity ->
+                Transaction(
+                    id = entity.id,
+                    customerName = entity.customerName,
+                    item = entity.item,
+                    amount = entity.amount,
+                    date = entity.date,
+                    status = entity.status
+                )
+
+            }
+        }
+    }
+}
