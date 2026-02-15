65ced8dd8b05658d6a888c150cf075967eb02029
Akeel shafi <akeelshafi20@gmail.com>
2026-02-15 17:58:09 +0530

    working on vendor side application

--- app/src/main/java/com/akeel/aitbaar/ui/vendor/dashboard/VendorDashboardFragment.kt
+++ app/src/main/java/com/akeel/aitbaar/ui/vendor/dashboard/VendorDashboardFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.akeel.aitbaar.R
-import com.akeel.aitbaar.data.model.Status
-import com.akeel.aitbaar.data.model.Transaction
+import com.akeel.aitbaar.data.repository.TransactionRepository
+import com.akeel.aitbaar.ui.vendor.VendorNavHelper
 import com.akeel.aitbaar.ui.vendor.transaction.TransactionAdapter
+import kotlinx.coroutines.launch
 
 class VendorDashboardFragment : Fragment() {
 
@@ -26,6 +29,15 @@ class VendorDashboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        VendorNavHelper.setup(this, view)
+        VendorNavHelper.highlight(this, view, R.id.iconHome)
+
+
+        view.findViewById<ImageView>(R.id.iconHome)
+            .setColorFilter(requireContext().getColor(R.color.blue))
+
+
 
         val recycler = view.findViewById<RecyclerView>(R.id.rvRecentTransactions)
         val addTransactionButton = view.findViewById<View>(R.id.btnAddTransaction)
@@ -42,31 +54,41 @@ class VendorDashboardFragment : Fragment() {
         }
 
         recycler.layoutManager = LinearLayoutManager(requireContext())
-        recycler.adapter = TransactionAdapter(getRecentTransactions())
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            TransactionRepository.getAllTransactions().collect { transactions ->
+
+                // Show only latest 3 on dashboard
+                val recentTransactions = transactions.take(6)
+
+                recycler.adapter = TransactionAdapter(recentTransactions)
+            }
+        }
+
     }
 
     // -------- ALL DUMMY TRANSACTIONS --------
-    private fun getDummyTransactions(): List<Transaction> {
-        return listOf(
-            Transaction("Akeel", "Milk + Bread", 5000, "06 Feb 2026", Status.ACCEPTED),
-            Transaction("Rafiq", "Rice", 80, "05 Feb 2026", Status.PENDING),
-            Transaction("Imran", "Eggs", 60, "04 Feb 2026", Status.REJECTED),
-            Transaction("Akeel", "Milk + Bread", 120, "03 Feb 2026", Status.ACCEPTED),
-            Transaction("Rafiq", "Rice", 80, "02 Feb 2026", Status.PENDING),
-            Transaction("Imran", "Eggs", 60, "01 Feb 2026", Status.REJECTED),
-            Transaction("Akeel", "Milk + Bread", 120, "31 Jan 2026", Status.ACCEPTED),
-            Transaction("Rafiq", "Rice", 80, "30 Jan 2026", Status.PENDING),
-            Transaction("Imran", "Eggs", 60, "29 Jan 2026", Status.REJECTED)
-        )
-    }
+    /* private fun getDummyTransactions(): List<Transaction> {
+         return listOf(
+             Transaction("Akeel", "Milk + Bread", 5000, "06 Feb 2026", Status.ACCEPTED),
+             Transaction("Rafiq", "Rice", 80, "05 Feb 2026", Status.PENDING),
+             Transaction("Imran", "Eggs", 60, "04 Feb 2026", Status.REJECTED),
+             Transaction("Akeel", "Milk + Bread", 120, "03 Feb 2026", Status.ACCEPTED),
+             Transaction("Rafiq", "Rice", 80, "02 Feb 2026", Status.PENDING),
+             Transaction("Imran", "Eggs", 60, "01 Feb 2026", Status.REJECTED),
+             Transaction("Akeel", "Milk + Bread", 120, "31 Jan 2026", Status.ACCEPTED),
+             Transaction("Rafiq", "Rice", 80, "30 Jan 2026", Status.PENDING),
+             Transaction("Imran", "Eggs", 60, "29 Jan 2026", Status.REJECTED)
+         )
+     }
 
-    // -------- ONLY RECENT 5 FOR DASHBOARD --------
-    private fun getRecentTransactions(): List<Transaction> {
-        val all = getDummyTransactions()
-        return if (all.size > 5) {
-            all.take(5)
-        } else {
-            all
-        }
-    }
+     // -------- ONLY RECENT 5 FOR DASHBOARD --------
+     private fun getRecentTransactions(): List<Transaction> {
+         val all = getDummyTransactions()
+         return if (all.size > 5) {
+             all.take(5)
+         } else {
+             all
+         }
+     }*/
 }
