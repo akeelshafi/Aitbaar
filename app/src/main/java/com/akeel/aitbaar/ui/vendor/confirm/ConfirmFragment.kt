b6ec832ad6cf8fccbab6658f031f8130d3d88b6f
Akeel shafi <akeelshafi20@gmail.com>
2026-03-02 22:26:00 +0530

    working on vendor side application

--- app/src/main/java/com/akeel/aitbaar/ui/vendor/confirm/ConfirmFragment.kt
+++ app/src/main/java/com/akeel/aitbaar/ui/vendor/confirm/ConfirmFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.navigation.fragment.findNavController
 import com.akeel.aitbaar.R
 import com.akeel.aitbaar.ui.vendor.VendorNavHelper
 
