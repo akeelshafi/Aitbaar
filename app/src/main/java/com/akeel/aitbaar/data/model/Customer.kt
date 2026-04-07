8d8092bf73ac0c6dcce4a670738e7603208eb0f4
Akeel Shafi <140949439+akeelshafi@users.noreply.github.com>
2026-04-07 23:09:19 +0530

    Use real contacts in customer selector with Aitbaar/invite states

--- app/src/main/java/com/akeel/aitbaar/data/model/Customer.kt
+++ app/src/main/java/com/akeel/aitbaar/data/model/Customer.kt
@@ -2,6 +2,7 @@ package com.akeel.aitbaar.data.model
 
 data class Customer(
     val name: String,
-    val phone: String
+    val phone: String,
+    val isOnAitbaar: Boolean = false,
+    val aitbaarName: String? = null
 )
-
